6b7ce3ee4b0173e693f4485cf98358ad7d7ae726
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2020-10-17 12:23:06 +0200

    Updates

---
+++ precache-manifest.6362a26f96eb2252267e1830ab7d528f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5626c0df28cd3a8c488ea592f4616f",
+    "url": "/ailo-the-boss/index.html"
+  },
+  {
+    "revision": "fde77db11cb25b9b3c6f",
+    "url": "/ailo-the-boss/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "342c8db49be4cc5a59c7",
+    "url": "/ailo-the-boss/static/js/2.4d6ff6be.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ailo-the-boss/static/js/2.4d6ff6be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde77db11cb25b9b3c6f",
+    "url": "/ailo-the-boss/static/js/main.dbcf4e55.chunk.js"
+  },
+  {
+    "revision": "2775e845769716b1cc9d",
+    "url": "/ailo-the-boss/static/js/runtime-main.09a30901.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ailo-the-boss/static/media/logo.5d5d9eef.svg"
+  }
+]);
